c9e8548719e6097aa561208a07d7df4db07f5e44
Pavel Metelsky <p.metelsky@itransition.com>
2022-02-18 22:04:27 +0300

    Init unit tests

---
+++ Piano.Api/Piano.BusinessLogic.IntegrationTests/InMemorySeedDataFixture.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Piano.Database;
+using System;
+
+namespace Piano.BusinessLogic.IntegrationTests
+{
+    public class InMemorySeedDataFixture : IDisposable
+    {
+        public PianoContext _pianoContext { get; private set; }
+
+        public InMemorySeedDataFixture()
+        {
+            var options = new DbContextOptionsBuilder<PianoContext>()
+                .UseInMemoryDatabase(databaseName: "Test")
+                .Options;
+
+            _pianoContext = new PianoContext(options);
+        }
+
+        public void AddUser(string email, string username, Guid userId, string password)
+        {
+            _pianoContext.Users.Add(new Entities.User { Email = email, Username = username, UserId = userId, Password = password });
+            _pianoContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _pianoContext.Dispose();
+        }
+    }
+}
--- Piano.Api/Piano.BusinessLogic/Queries/Users/GetUsers/GetUsersQuery.cs
+++ Piano.Api/Piano.BusinessLogic/Queries/Users/GetUsers/GetUsersQuery.cs
@@ -1,10 +1,5 @@
 ﻿using MediatR;
 using Piano.BusinessLogic.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Piano.BusinessLogic.Queries.Users.GetUsers
 {
