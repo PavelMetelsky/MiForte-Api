911c99c51ed0ecc86856e066e55c0ea92f1d040a
skalem <pimako2003@yandex.by>
2022-03-16 16:41:11 +0300

    Created regestration-main componnent

---
+++ Piano/src/app/regestration-main/regestration-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-regestration-main',
+  templateUrl: './regestration-main.component.html',
+  styleUrls: ['./regestration-main.component.scss']
+})
+export class RegestrationMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
