9739473e1c6b6673187fa9c9766cd8500b6bcbba
skalem <pimako2003@yandex.by>
2022-03-16 16:58:12 +0300

    added regestration-role component

--- Piano/src/app/app.module.ts
+++ Piano/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RegestrationComponent } from './regestration/regestration.component';
 import { RegestrationMainComponent } from './regestration-main/regestration-main.component';
+import { RegestrationRoleComponent } from './regestration-role/regestration-role.component';
 
 @NgModule({
-  declarations: [AppComponent, RegestrationComponent, RegestrationMainComponent],
+  declarations: [AppComponent, RegestrationComponent, RegestrationMainComponent, RegestrationRoleComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ Piano/src/app/regestration-role/regestration-role.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-regestration-role',
+  templateUrl: './regestration-role.component.html',
+  styleUrls: ['./regestration-role.component.scss']
+})
+export class RegestrationRoleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
