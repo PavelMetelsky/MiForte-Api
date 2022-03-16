c4c7866e4a50162e88f284146b4e809fd2883fad
skalem <pimako2003@yandex.by>
2022-03-16 16:44:24 +0300

    minor changes to app component

--- Piano/src/app/app.module.ts
+++ Piano/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RegestrationComponent } from './regestration/regestration.component';
+import { RegestrationMainComponent } from './regestration-main/regestration-main.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RegestrationComponent, RegestrationMainComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
