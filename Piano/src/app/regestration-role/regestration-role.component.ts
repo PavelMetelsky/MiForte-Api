1ff9bc5892d8fcfeae774e18bc72cdd3a1e086e7
skalem <pimako2003@yandex.by>
2022-03-17 17:50:19 +0300

    Made first part of regestration

--- Piano/src/app/regestration-role/regestration-role.component.ts
+++ Piano/src/app/regestration-role/regestration-role.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegestrationRoleComponent implements OnInit {
 
+  public isStudent = false;
+  public isMentor = false;
+  public enoughInfo = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  studentClick(){
+    this.isStudent = true;
+    this.isMentor = false;
+    this.enoughInfo = true;
+  }
+
+  mentorClick(){
+    this.isStudent = false;
+    this.isMentor = true;
+    this.enoughInfo = true;
   }
 
 }
